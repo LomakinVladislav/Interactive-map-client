e9da881626d416e7e0458a29435118bf5e9e0941
kilianAid <kilian_bs@mail.ru>
2024-03-14 18:56:33 +0500

    Изменение структуры сайта и изменение стиля

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
-<template>
-  <mapBlock></mapBlock>
+<template> 
+  <topPanel/>
+  <div class="main">
+    <leftPanel/>
+    <mapBlock/>
+  </div>
 </template>
 
 <script setup>
+import topPanel from "./components/Top-panel.vue";
 import mapBlock from "./components/Map-component.vue";
+import leftPanel from "./components/LeftPanel/Left-panel.vue"
 </script>
 
-<style scoped>
+<style lang="scss">
+#app {
+  width: 100%;
+  height: 100vh;
+  max-height: 100vh;
 
+  display: flex;
+  flex-direction: column;
+
+  .main {
+    display: flex;
+    flex-grow: 1;
+    max-height: calc(100vh - 65px);
+  }
+}
 </style>
